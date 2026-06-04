9125894879e5653db55ab70e01e5872742ddb473
SantiagoRojas12 <santaigojararojas@email.com>
2026-06-03 20:04:42 -0500

    SECRET_KEY a variable de entorno

--- config.py
+++ config.py
@@ -1,12 +1,10 @@
 import os
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'mi_clave_secreta_123'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev_key_local'
     
-    # Usa PostgreSQL en Render o SQLite local
     DATABASE_URL = os.environ.get('DATABASE_URL') or 'sqlite:///inventario.db'
     
-    # Render usa 'postgres://' pero SQLAlchemy necesita 'postgresql://'
     if DATABASE_URL.startswith('postgres://'):
         DATABASE_URL = DATABASE_URL.replace('postgres://', 'postgresql://', 1)
     
